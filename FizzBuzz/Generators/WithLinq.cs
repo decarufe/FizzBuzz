2cd0fd25d08c02ee213b230d454dca1af16c70e0
Eric De Carufel <eric.decarufel@gmail.com>
2018-12-26 22:37:05 -0500

    Refactro for Linq version

--- FizzBuzz/Generators/WithLinq.cs
+++ FizzBuzz/Generators/WithLinq.cs
@@ -22,10 +22,10 @@ namespace FizzBuzz.Generators
             Console.WriteLine("=== Linq with string.Join ===");
 
             var result = from source in Enumerable.Range(1, count)
-                let fizz = source%3 == 0 ? "Fizz" : ""
-                let buzz = source%5 == 0 ? "Buzz" : ""
-                let number = (fizz + buzz).Length == 0 ? source.ToString() : ""
-                select fizz + buzz + number;
+                let fizz = source%3 == 0 ? "Fizz" : string.Empty
+                let buzz = source%5 == 0 ? "Buzz" : string.Empty
+                let number = (fizz + buzz).Length == 0 ? source.ToString() : string.Empty
+                select String.Join(string.Empty, fizz, buzz, number);
             result.ForEach(_todo);
         }
     }
